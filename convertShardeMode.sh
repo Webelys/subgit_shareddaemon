f8eae1bd5eba8be1968edab4ade91e62165e493b
cam.lafit <cam.lafit@azerttyu.net>
2017-03-03 14:39:00 +0100

    Comment use  # not // (it's bash script)

--- convertShardeMode.sh
+++ convertShardeMode.sh
@@ -16,25 +16,25 @@ for repo_source in $(find $GIT_ROOT -iname *.git); do
     echo $repo_source
     repo_name=$(basename $repo_source .git)
 
-    //Is a subgit project ?
+    ##Is a subgit project ?
     if [ ! -f $repo_source/subgit/config ]; then
         echo "Ignore project ; subgit translation not enabled"
         continue
     fi
 
-    //Is as shared mode ?
+    ##Is as shared mode ?
     if [ $(grep -r 'daemon "shared"' $repo_source/subgit/config) ]; then
         echo "Shared daeamon yet enabled"
         continue
     fi
 
-    // Re Enable subgit configuration
+    ##Disable subgit configuration
     subgit uninstall $repo_source
 
-    //Set shared mode
+    ##Set shared mode
     echo "[daemon \"shared\"]" >> $repo_source/subgit/config
     echo "    directory = $GIT_ROOT/shared-daemon" >> $repo_source/subgit/config
 
-    // Re Enable subgit configuration
+    ##Re Enable subgit configuration
     subgit install $repo_source
 done
