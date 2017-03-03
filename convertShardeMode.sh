76e7eeb0d10cb85de9d301512129932a20ceed24
cam.lafit <cam.lafit@azerttyu.net>
2017-03-03 14:43:40 +0100

    Shellcheck control
    * Correct main warning from http://www.shellcheck.net/

--- convertShardeMode.sh
+++ convertShardeMode.sh
@@ -2,7 +2,7 @@
 
 . config.conf
 
-if [ -z $GIT_ROOT ]; then
+if [ -z "$GIT_ROOT" ]; then
     echo "Set a git repository first"
     exit 1
 fi
@@ -12,29 +12,29 @@ if [ ! -f "$PROJECT_ROOT/shared-daemon" ]; then
     cp shared-daemon.config "$GIT_ROOT/shared-daemon/config"
 fi
 
-for repo_source in $(find $GIT_ROOT -iname *.git); do
-    echo $repo_source
-    repo_name=$(basename $repo_source .git)
+for repo_source in $(find "$GIT_ROOT" -iname "*.git"); do
+    echo "$repo_source"
+    repo_name=$(basename "$repo_source" .git)
 
     ##Is a subgit project ?
-    if [ ! -f $repo_source/subgit/config ]; then
+    if [ ! -f "$repo_source/subgit/config" ]; then
         echo "Ignore project ; subgit translation not enabled"
         continue
     fi
 
     ##Is as shared mode ?
-    if [ $(grep -r 'daemon "shared"' $repo_source/subgit/config) ]; then
+    if [ $(grep -r 'daemon "shared"' "$repo_source/subgit/config") ]; then
         echo "Shared daeamon yet enabled"
         continue
     fi
 
     ##Disable subgit configuration
-    subgit uninstall $repo_source
+    subgit uninstall "$repo_source"
 
     ##Set shared mode
-    echo "[daemon \"shared\"]" >> $repo_source/subgit/config
-    echo "    directory = $GIT_ROOT/shared-daemon" >> $repo_source/subgit/config
+    echo "[daemon \"shared\"]" >> "$repo_source/subgit/config"
+    echo "    directory = $GIT_ROOT/shared-daemon" >> "$repo_source/subgit/config"
 
     ##Re Enable subgit configuration
-    subgit install $repo_source
+    subgit install "$repo_source"
 done
