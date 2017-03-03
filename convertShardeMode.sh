bdc7ac8aa2375cef150d49ca699a95d90f969b87
cam.lafit <cam.lafit@azerttyu.net>
2017-03-03 14:05:36 +0100

    Add daemon shared option in subgit configuration
    * to each subgit project add shared dameon directive

---
+++ convertShardeMode.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+
+. config.conf
+
+if [ -z $GIT_ROOT ]; then
+    echo "Set a git repository first"
+    exit 1
+fi
+
+
+
+for repo_source in $(find $GIT_ROOT -iname *.git); do
+    echo $repo_source
+    repo_name=$(basename $repo_source .git)
+
+    //Is a subgit project ?
+    if [ ! -f $repo_source/subgit/config ]; then
+        echo "Ignore project ; subgit translation not enabled"
+        continue
+    fi
+
+    //Is as shared mode ?
+    if [ $(grep -r 'daemon "shared"' $repo_source/subgit/config) ]; then
+        echo "Shared daeamon yet enabled"
+        continue
+    fi
+
+    //Set shared mode
+    echo "[daemon \"shared\"]" >> $repo_source/subgit/config
+    echo "    directory = $GIT_ROOT/shared-daemon" >> $repo_source/subgit/config
+done
